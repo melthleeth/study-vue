80734c7a95c28cf30873aad3097a5fd52ebcd38f
Hyejin Lee <hichjay@gmail.com>
2021-01-21 02:05:46 +0900

    Section 16 진행중 - filter까지 완료

--- Section 16/main-prj-09-filtering-coaches/src/main.js
+++ Section 16/main-prj-09-filtering-coaches/src/main.js
@@ -3,11 +3,11 @@ import { createApp } from 'vue';
 import router from './router.js';
 import store from './store/index.js';
 import App from './App.vue';
-import BaseCard from  './components/ui/BaseCard.vue';
-import BaseButton from  './components/ui/BaseButton.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
 
-const app = createApp(App);
+const app = createApp(App)
 
 app.use(router);
 app.use(store);
